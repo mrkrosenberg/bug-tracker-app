1910d34079bdb004c59a8f254eddb2eadca6aa77
Mark <mrk.rosenberg@gmail.com>
2020-10-27 13:12:53 -0600

    adds component to authenticate routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // Router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Token decoder
+import jwtDecode from 'jwt-decode';
+
 // Pages
 import home from './pages/home';
 import login from './pages/login';
@@ -8,59 +11,32 @@ import signup from './pages/signup';
 
 // Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 
 // Stylesheet
 import './App.css';
-// import theme from './styles/globalStyles';
+import themeFile from './styles/globalStyles';
 
 // import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import JwtDecode from 'jwt-decode';
 
-const theme = createMuiTheme({
+const theme = createMuiTheme(themeFile);
 
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00dcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
+const authToken = localStorage.FBIdToken;
+let authenticated;
+
+if(authToken) {
+
+    const decodedToken = jwtDecode(authToken);
+    if(decodedToken.exp * 1000 < Date.now()) {
+      window.location.href="/login";
+      authenticated = false;
+    } else {
+      authenticated = true;
     }
-  },
-  typography: {
-    useNextVariants: true
-  },
-  // form: {
-  //   textAlign: 'center'
-  // },
-  // pageTitle: {
-  //     margin: '20px auto 20px auto'
-  // },
-  // image: {
-  //     margin: '10px auto 10px auto'
-  // },
-  // textField: {
-  //     margin: '10px auto 10px auto'
-  // },
-  // button: {
-  //     position: 'relative',
-  //     margin: '20px auto 20px auto'
-  // },
-  // customError: {
-  //     color: '#ff3d00',
-  //     fontSize: '0.8rem',
-  //     marginTop: 10
-  // },
-  // progress: {
-  //     position: 'absolute'
-  // }
-});
+};
 
 function App() {
   return (
@@ -71,8 +47,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <Route path="/login" component={login} />
-              <Route path="/signup" component={signup} />
+              <AuthRoute path="/login" component={login} authenticated={authenticated} />
+              <AuthRoute path="/signup" component={signup} authenticated={authenticated} />
             </Switch>
           </div>
         </Router>
